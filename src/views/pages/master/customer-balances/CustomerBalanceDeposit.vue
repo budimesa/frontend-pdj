608365b722d9b17cbe5a62733caf3e032833137e
budimesa <budihmesa@gmail.com>
2024-10-10 13:15:30 +0700

    add customer balance

---
+++ src/views/pages/master/customer-balances/CustomerBalanceDeposit.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <h1 class="text-2xl font-bold mb-4">Customer Balance Deposit</h1>
+        <Toolbar class="mb-6">
+            <template #start>
+                <Button label="New" icon="pi pi-plus" severity="success" class="mr-2" @click="openNew" />
+            </template>
+            <template #end>
+                <Button label="Export" icon="pi pi-upload" severity="help" />
+            </template>
+        </Toolbar>
+    </div>
+</template>
+
+
+<script setup>
+</script>
